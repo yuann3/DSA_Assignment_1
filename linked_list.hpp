df09462a4e751c4167a4b082c49ce764ba715e53
Yiyuan <etherealdt@gmail.com>
2024-06-12 12:21:02 +0800

    linked_list

--- linked_list.hpp
+++ linked_list.hpp
@@ -5,6 +5,11 @@
  */
 
 #include "empty_collection_exception.h"
+
+/*
+* Precondition:    None
+* Postcondition:   A new LinkedList is created, with all variables initialised.
+*/
 template <typename T>
 LinkedList<T>::LinkedList() : head(new Node<T>()), tail(new Node<T>()), count(0)
 {
@@ -13,6 +18,10 @@ LinkedList<T>::LinkedList() : head(new Node<T>()), tail(new Node<T>()), count(0)
 	current = head;
 }
 
+/*
+* Precondition:    None
+* Postcondition:   The LinkedList is destroyed and all associated memory is freed.
+*/
 template <typename T>
 LinkedList<T>::~LinkedList()
 {
@@ -21,6 +30,12 @@ LinkedList<T>::~LinkedList()
 	delete tail;
 }
 
+/*
+* The supplied data is inserted at the front of the list.
+* 
+* Precondition:    The supplied data is valid.
+* Postcondition:   The first data item is updated and current points to the new node.
+*/
 template <typename T>
 void LinkedList<T>::push_front(const T &data)
 {
@@ -32,6 +47,12 @@ void LinkedList<T>::push_front(const T &data)
 	count++;
 }
 
+/*
+* The supplied data is inserted at the end of the list.
+* 
+* Precondition:    The supplied data is valid.
+* Postcondition:   The last data item is updated and current points to the new node.
+*/
 template <typename T>
 void LinkedList<T>::push_back(const T &data)
 {
@@ -43,6 +64,12 @@ void LinkedList<T>::push_back(const T &data)
 	count++;
 }
 
+/*
+* The supplied data is inserted before the current node.
+* 
+* Precondition:    Current points to the node after the insertion point.
+* Postcondition:   A new node has been added and current points to the new node.
+*/
 template <typename T>
 void LinkedList<T>::insert(const T &data)
 {
@@ -56,6 +83,12 @@ void LinkedList<T>::insert(const T &data)
 	count++;
 }
 
+/*
+* Remove the first data element from the list. An exception should be thrown if the list is empty.
+* 
+* Precondition:    The list is not empty.
+* Postcondition:   The first data element has been removed, reducing the count of Nodes by 1. Current points to head.
+*/
 template <typename T>
 T LinkedList<T>::pop_front()
 {
@@ -70,6 +103,12 @@ T LinkedList<T>::pop_front()
 	return data;
 }
 
+/*
+* Remove the last data element from the list. An exception should be thrown if the list is empty.
+* 
+* Precondition:    The list is not empty.
+* Postcondition:   The last data element has been removed, reducing the count of Nodes by 1. Current points to head.
+*/
 template <typename T>
 T LinkedList<T>::pop_back()
 {
@@ -84,6 +123,13 @@ T LinkedList<T>::pop_back()
 	return data;
 }
 
+/*
+* Remove the item pointed to by current from the list. An exception should be thrown if the list is empty or 
+* if current is pointing to a sentinel node.
+* 
+* Precondition:    The list is not empty and the current pointer is not pointing to a sentinel node.
+* Postcondition:   The data element pointed to by current has been removed, reducing the count of Nodes by 1. Current points to head.
+*/
 template <typename T>
 T LinkedList<T>::remove()
 {
@@ -99,6 +145,12 @@ T LinkedList<T>::remove()
 	return data;
 }
 
+/*
+* Clears all data elements from the list, leaving the sentinel nodes intact.
+* 
+* Precondition:    None
+* Postcondition:   All data elements have been removed. Sentinels should not be removed. Count should be reset.
+*/
 template <typename T>
 void LinkedList<T>::clear()
 {
@@ -114,6 +166,12 @@ void LinkedList<T>::clear()
 	count = 0;
 }
 
+/*
+* Return a reference to the first data element in the list - not the sentinel. An exception should be thrown if the list is empty.
+* 
+* Precondition:    The list is not empty.
+* Postcondition:   A reference to the first data element is returned.
+*/
 template <typename T>
 T &LinkedList<T>::front() const
 {
@@ -122,6 +180,12 @@ T &LinkedList<T>::front() const
 	return head->get_next()->get_data();
 }
 
+/*
+* Return a reference to the last data element in the list - not the sentinel. An exception should be thrown if the list is empty.
+* 
+* Precondition:    The list is not empty.
+* Postcondition:   A reference to the last data element is returned.
+*/
 template <typename T>
 T &LinkedList<T>::back() const
 {
@@ -130,6 +194,12 @@ T &LinkedList<T>::back() const
 	return tail->get_prev()->get_data();
 }
 
+/*
+* Return a reference to the data element pointed to by current. An exception should be thrown if the list is empty or current points to a sentinel.
+* 
+* Precondition:    The list is not empty and the current pointer is not pointing to a sentinel node.
+* Postcondition:   A reference to the current data element is returned.
+*/
 template <typename T>
 T &LinkedList<T>::get_current() const
 {
@@ -138,18 +208,36 @@ T &LinkedList<T>::get_current() const
 	return current->get_data();
 }
 
+/*
+* Set the current pointer to the node after head, even if this is tail.
+* 
+* Precondition:    None
+* Postcondition:   The current pointer is set to the node after head.
+*/
 template <typename T>
 void LinkedList<T>::begin()
 {
 	current = head->get_next();
 }
 
+/*
+* Set the current pointer to the node before tail head, even if this is head.
+* 
+* Precondition:    None
+* Postcondition:   The current pointer is set to the node before tail.
+*/
 template <typename T>
 void LinkedList<T>::end()
 {
 	current = tail->get_prev();
 }
 
+ /*
+* Move the current pointer forward, if valid. Otherwise, nothing happens.
+* 
+* Precondition:    None
+* Postcondition:   The current pointer is set to the next node, if applicable.
+*/
 template <typename T>
 void LinkedList<T>::forward()
 {
@@ -157,6 +245,12 @@ void LinkedList<T>::forward()
 		current = current->get_next();
 }
 
+/*
+* Move the current pointer backward, if valid. Otherwise, nothing happens.
+* 
+* Precondition:    None
+* Postcondition:   The current pointer is set to the previous node, if applicable.
+*/  
 template <typename T>
 void LinkedList<T>::backward()
 {
@@ -164,18 +258,36 @@ void LinkedList<T>::backward()
 		current = current->get_prev();
 }
 
+/*
+* Return the count of the number of nodes in the list, excluding sentinels.
+* 
+* Precondition:    None
+* Postcondition:   The number of (true) nodes is returned.
+*/ 
 template <typename T>
 int LinkedList<T>::size() const
 {
 	return count;
 }
 
+/*
+* Return true if the list is empty, false otherwise.
+* 
+* Precondition:    None
+* Postcondition:   None
+*/  
 template <typename T>
 bool LinkedList<T>::empty() const
 {
 	return count == 0;
 }
 
+/*
+* Set the current pointer to the node containing the supplied data. Otherwise, nothing happens.
+* 
+* Precondition:    None
+* Postcondition:   current points to the first node storing the target, and true is returned.
+*/ 
 template <typename T>
 bool LinkedList<T>::search(const T &target)
 {
